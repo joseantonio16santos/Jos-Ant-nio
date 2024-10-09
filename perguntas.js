8b26453d17bee75151f47db4477daafb59630b47
joseantonio16santos <162702323+joseantonio16santos@users.noreply.github.com>
2024-10-09 09:27:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Por que o céu é azul?',
+    'O céu é azul devido à dispersão da luz solar na atmosfera da Terra. Quando a luz do sol entra na atmosfera, as ondas de luz azul são espalhadas em todas as direções mais do que as ondas de luz de outras cores.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é o maior organismo vivo da Terra?',
+    'O maior organismo vivo é um fungo chamado Armillaria ostoyae, que se estende por cerca de 3,4 quilômetros quadrados em Oregon, EUA.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Como os furacões se formam?',
+    'Eles se formam sobre águas quentes do oceano, quando a umidade se eleva e a rotação da Terra ajuda a criar a organização das tempestades.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Quais mudanças importantes ocorreram durante o reinado de Rainha Elisabeth II ?',
+    'Seu reinado testemunhou a descolonização da África e do Caribe, a evolução da sociedade britânica e o avanço das tecnologias.'
+)
